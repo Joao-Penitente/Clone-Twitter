6737079da8f3cb83b55e4bdcff39b739e2185cde
joaovitor-penitente <joaov.penitente@gmail.com>
2023-02-14 14:04:00 -0300

    feat: Twitter component created

---
+++ src/components/Tweet/Tweet.tsx
@@ -0,0 +1,39 @@
+import { ArrowsClockwise, ChatCircle, Heart } from 'phosphor-react'
+import { Link } from 'react-router-dom'
+import './Tweet.css'
+
+interface TweetProps {
+    content: string
+}
+
+export function Tweet(props : TweetProps) {
+    return (
+        <Link to= "/status" className="tweet">
+           <img src="https://github.com/joaovitor-penitente.png" alt="" /> 
+           <div className="tweet-content">
+                <div className="tweet-content-header">
+                    <strong>Jota</strong>
+                    <span> @joaovitor2727</span>
+                </div>
+                <p>
+                    {props.content}
+                </p>
+                <div className="tweet-content-footer">
+                    <button type="button">
+                        <ChatCircle />
+                        20   
+                    </button>
+                    <button type="button">
+                        <ArrowsClockwise />
+                        20   
+                    </button>
+                    <button type="button">
+                        <Heart />
+                        20   
+                    </button>
+                    
+                </div>
+           </div>
+        </Link>
+    )
+}
